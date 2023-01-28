91ed6bb133658e812036b3ca390a1e3702f71e35
luozx <604679490@qq.com>
2023-01-28 17:10:39 +0800

    feat:首页头部底部页面实现

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,88 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe9fb;</span>
+      北京理工大学国防科技园北京理工大学国防科技园北京理工大学国防科技园
+      <span class="iconfont position__notice">&#xe63f;</span>
+    </div>
+  </div>
+  <div class='docker'>
+    <div class="docker__item">
+      <div class="iconfont docker__item--active">&#xe648;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe634;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe6ab;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe660;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+@import './style/viriables.scss';
+@import './style/mixins.scss';
+  .wrapper {
+    display: flex;
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: .5rem;
+    right: 0;
+    padding: 0 .18rem;
+  }
+  .position {
+    position: relative;
+    padding: .16rem .24rem .16rem 0;
+    line-height: .22rem;
+    width : 100%;
+    font-size: .16rem;
+    color: $content-fontcolor;
+    @include ellipsis;
+    .position__icon {
+      position: relative;
+      font-size: .2rem;
+      top: .01rem;
+    }
+    .position__notice {
+      position: absolute;
+      font-size: .2rem;
+      right: 0;
+      top: .1rem;
     }
   }
-}
+  .docker {
+    display: flex;
+    position: absolute;
+    box-sizing: border-box;
+    padding: 0 .18rem;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 0.49rem;
+    border-top: 1px solid #F1F1F1;
+    color: $content-fontcolor;
+    &__item {
+      flex : 1;
+      text-align: center;
+      &--active {
+        color: #1FA4FC;
+      }
+    };
+    &__title {
+      font-size : 0.2rem;
+      transform: scale(0.5, 0.5);
+      transform-origin: center top ;
+    }
+  };
+  .iconfont {
+    padding: .07rem 0 .02rem 0;
+  };
 </style>
